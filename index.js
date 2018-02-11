bbd5ee2ba2c09f3d666da905e8844b478d811672
codearvin <d51279679@163.com>
2018-02-11 19:49:18 +0800

    const let to var

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const $ = function (id) {
+var $ = function (id) {
   return document.getElementById(id);
 }
 window.onload = function () {
 
   // page1
-  const page = $('page'),
+  var page = $('page'),
         flag = $('flag'),
         line = $('line'),
         random = $('random'),
@@ -12,9 +12,9 @@ window.onload = function () {
         show_answer = $('show_answer'),
         len = data.length;
 
-  const set_answer = function (data, l) {
-    const create_div = function (index, text, color) {
-      let div = document.createElement('div');
+  var set_answer = function (data, l) {
+    var create_div = function (index, text, color) {
+      var div = document.createElement('div');
       div.innerHTML = '<span style="display:inline-block;width:30px">' + (index + 1) + '. </span><div style="display:inline-block;width:370px">' + text + '</div>';
       div.style.width = '400px';
       div.style.margin = '0 auto';
@@ -25,7 +25,7 @@ window.onload = function () {
     };
     show_answer.innerHTML = '';
     data.forEach(function (text, index) {
-      let color = false;
+      var color = false;
       if (index === l) {
         color = true;
       }
@@ -33,13 +33,13 @@ window.onload = function () {
     });
   };
 
-  const random_question = function () {
+  var random_question = function () {
     show_answer.style.visibility = 'hidden';
 
-    const p = Math.floor(Math.random() * len);
-    const f = Math.random() >= 0.5 ? '正' : '倒';
-    const p_len = data[p].length;
-    const l = Math.floor(Math.random() * p_len);
+    var p = Math.floor(Math.random() * len);
+    var f = Math.random() >= 0.5 ? '正' : '倒';
+    var p_len = data[p].length;
+    var l = Math.floor(Math.random() * p_len);
 
     page.innerText = 2 * (p + 1) + 1;
     flag.innerText = f;
@@ -59,16 +59,16 @@ window.onload = function () {
   random_question();
 
   // page2
-  const question2 = $('question2'),
+  var question2 = $('question2'),
         random2 = $('random2'),
         answer2 = $('answer2'),
         page_2 = $('page_2'),
         line2 = $('line2');
-  const random_question2 = function () {
+  var random_question2 = function () {
     show_answer2.style.visibility = 'hidden';
-    const p = Math.floor(Math.random() * len);
-    const p_len = data[p].length;
-    const l = Math.floor(Math.random() * p_len);
+    var p = Math.floor(Math.random() * len);
+    var p_len = data[p].length;
+    var l = Math.floor(Math.random() * p_len);
 
     question2.innerText = data[p][l];
     page_2.innerText = 2 * (p + 1) + 1;
@@ -86,14 +86,14 @@ window.onload = function () {
   random_question2();
 
   // page3
-  const pi_dom = $('pi'),
+  var pi_dom = $('pi'),
         random3 = $('random3'),
         answer3 = $('answer3'),
         show_answer3 = $('show_answer3');
 
-  const random_question3 = function () {
+  var random_question3 = function () {
     show_answer3.style.visibility = 'hidden';
-    let r = Math.floor(Math.random() * 1000);
+    var r = Math.floor(Math.random() * 1000);
 
     pi_dom.innerText = r + 1;
     show_answer3.innerText = pi[r];
@@ -110,38 +110,38 @@ window.onload = function () {
   random_question3();
 
   // page4
-  const show_answer4 = $('show_answer4');
-  const random_name = function () {
+  var show_answer4 = $('show_answer4');
+  var random_name = function () {
     return name[Math.floor(Math.random() * name.length)]
   };
-  const random_number = function () {
+  var random_number = function () {
     return Math.floor(Math.random() * 10);
   };
-  const random_tag = function () {
+  var random_tag = function () {
     return tag[Math.floor(Math.random() * 32)];
   }
-  const random_char = function () {
+  var random_char = function () {
     return char[Math.floor(Math.random() * 26)];
   }
   $('name').addEventListener('click', function () {
-    let text = '';
-    for (let i = 0; i < 60; i++) {
+    var text = '';
+    for (var i = 0; i < 60; i++) {
       text += '<div>' + random_name() + '</div>';
     }
     show_answer4.innerHTML = text;
   });
 
   $('num').addEventListener('click', function () {
-    let text = '';
-    for (let i = 0; i < 60; i++) {
+    var text = '';
+    for (var i = 0; i < 60; i++) {
       text += '<div class="num">' + random_number() + '</div>';
     }
     show_answer4.innerHTML = text;
   });
 
   $('all').addEventListener('click', function () {
-    let text = '', r;
-    for (let i = 0; i < 25; i++) {
+    var text = '', r;
+    for (var i = 0; i < 25; i++) {
       r = Math.random();
       if (r < 0.25) {
         text += '<div>' + random_tag() + '</div>';
@@ -160,9 +160,9 @@ window.onload = function () {
 
   // 页面tab
 
-  for (let i = 0; i < 4; i++) {
+  for (var i = 0; i < 4; i++) {
     $('page'+(i+1)).addEventListener('click', function () {
-      for (let j = 0; j < 4; j++) {
+      for (var j = 0; j < 4; j++) {
         $('page'+(j+1)+'_').style.display = (i === j) ? 'block' : 'none';
       }
     });
